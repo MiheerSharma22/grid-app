c4eac184a18873b71c5d76d6db27ad47dbcbc231
MiheerSharma22 <miheer.sharma1@gmail.com>
2026-05-08 23:57:25 +0530

    added leaderboard event in socket and its assocaited logic

--- server/socket.js
+++ server/socket.js
@@ -1,6 +1,8 @@
 const { Server } = require("socket.io");
 const Cell = require("./models/Cell");
 
+const onlineUsers = new Map();
+
 function initSocket(server) {
   const io = new Server(server, {
     cors: {
@@ -11,10 +13,22 @@ function initSocket(server) {
   io.on("connection", async (socket) => {
     console.log("User connected:", socket.id);
 
-    // Send full grid initially
+    // Initial grid
     const cells = await Cell.find();
+
     socket.emit("init_grid", cells);
 
+    // Register user
+    socket.on("register_user", ({ userId, color }) => {
+      onlineUsers.set(userId, {
+        socketId: socket.id,
+        color,
+      });
+
+      emitLeaderboard(io);
+    });
+
+    // Claim cell
     socket.on("claim_cell", async ({ x, y, userId, color }) => {
       try {
         const newCell = await Cell.create({
@@ -24,10 +38,13 @@ function initSocket(server) {
           color,
         });
 
+        // Broadcast the updated cell to all clients
         io.emit("cell_updated", newCell);
+
+        emitLeaderboard(io);
       } catch (err) {
         if (err.code === 11000) {
-          console.log("Cell already claimed by someone else");
+          console.log("Already claimed");
         } else {
           console.error(err);
         }
@@ -35,9 +52,43 @@ function initSocket(server) {
     });
 
     socket.on("disconnect", () => {
-      console.log("User disconnected");
+      for (const [userId, value] of onlineUsers) {
+        if (value.socketId === socket.id) {
+          onlineUsers.delete(userId);
+          break;
+        }
+      }
+
+      emitLeaderboard(io);
+
+      console.log("Disconnected:", socket.id);
     });
   });
+}
+
+async function emitLeaderboard(io) {
+  const cells = await Cell.find();
+
+  const leaderboardMap = {};
+
+  cells.forEach((cell) => {
+    if (!leaderboardMap[cell.ownerId]) {
+      leaderboardMap[cell.ownerId] = {
+        userId: cell.ownerId,
+        color: cell.color,
+        blocks: 0,
+        online: onlineUsers.has(cell.ownerId),
+      };
+    }
+
+    leaderboardMap[cell.ownerId].blocks += 1;
+  });
+
+  const leaderboard = Object.values(leaderboardMap).sort(
+    (a, b) => b.blocks - a.blocks,
+  );
+
+  io.emit("leaderboard_update", leaderboard);
 }
 
 module.exports = { initSocket };
